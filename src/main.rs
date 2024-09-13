086c3704a53e7bf269e783439896c0854d23bd02
adriannic <spamadriannic@gmail.com>
2024-09-13 22:54:21 +0200

    fix(hypr): fix switching to empty workspace

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,59 @@ use hyprland::dispatch::*;
 use hyprland::prelude::*;
 
 fn main() -> hyprland::Result<()> {
-    let selected_ws = match env::args().nth(1) {
-        Some(value) => value.parse::<i32>().expect("Workspace identifier expected"),
-        None => panic!("Workspace identifier expected"),
-    };
+    let selected_ws_id = env::args()
+        .nth(1)
+        .expect("Workspace identifier expected")
+        .parse::<i32>()
+        .expect("Workspace identifier expected");
 
-    if selected_ws == Workspace::get_active()?.id {
+    // Do nothing if the selected workspace is the current workspace
+    if selected_ws_id == Workspace::get_active()?.id {
         return Ok(());
     }
 
-    match Monitors::get()?
+    // Swap workspaces if the selected workspace is active in another monitor
+    if let Some(mon) = Monitors::get()?
         .iter()
-        .find(|mon| mon.active_workspace.id == selected_ws)
+        .find(|mon| mon.active_workspace.id == selected_ws_id)
     {
-        Some(mon) => hyprland::dispatch!(
+        hyprland::dispatch!(
             SwapActiveWorkspaces,
             MonitorIdentifier::Current,
             MonitorIdentifier::Id(mon.id)
-        )?,
-        None => {
-            hyprland::dispatch!(
-                MoveWorkspaceToMonitor,
-                WorkspaceIdentifier::Id(selected_ws),
-                MonitorIdentifier::Current
-            )?;
-            hyprland::dispatch!(Workspace, WorkspaceIdentifierWithSpecial::Id(selected_ws))?;
-        }
+        )?
     }
+
+    // If the workspace exists move it to the current monitor
+    let _ = hyprland::dispatch!(
+        MoveWorkspaceToMonitor,
+        WorkspaceIdentifier::Id(selected_ws_id),
+        MonitorIdentifier::Current
+    );
+
+    // Switch to the desired workspace
+    hyprland::dispatch!(
+        Workspace,
+        WorkspaceIdentifierWithSpecial::Id(selected_ws_id)
+    )?;
+
+    // match Monitors::get()?
+    //     .iter()
+    //     .find(|mon| mon.active_workspace.id == selected_ws)
+    // {
+    //     Some(mon) => hyprland::dispatch!(
+    //         SwapActiveWorkspaces,
+    //         MonitorIdentifier::Current,
+    //         MonitorIdentifier::Id(mon.id)
+    //     )?,
+    //     None => {
+    //         hyprland::dispatch!(
+    //             MoveWorkspaceToMonitor,
+    //             WorkspaceIdentifier::Id(selected_ws),
+    //             MonitorIdentifier::Current
+    //         )?;
+    //         hyprland::dispatch!(Workspace, WorkspaceIdentifierWithSpecial::Id(selected_ws))?;
+    //     }
+    // }
     Ok(())
 }
